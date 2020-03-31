7a03261b52a548230213b1d0d1376ceaa1dc4776
Rob Lohmann <roblohman@gmail.com>
2020-03-31 14:22:49 +0200

    improved extension method for parsing enumerations

--- CodeCube.Core/Extensions/IntegerExtensions.cs
+++ CodeCube.Core/Extensions/IntegerExtensions.cs
@@ -9,11 +9,22 @@ namespace CodeCube.Core.Extensions
         /// </summary>
         /// <typeparam name="TEnum">The type of enum which the string should be parsed into.</typeparam>
         /// <param name="value">The integer value to parse.</param>
+        /// <returns>If parsing succeeds, then the parsed enum value will be returned. Otherwise the defaultvalue will be returned.</returns>
+        public static TEnum? TryParseEnum<TEnum>(this int value) where TEnum : struct
+        {
+            return EnumHelper.TryParseEnum<TEnum>(value, null);
+        }
+
+        /// <summary>
+        /// Try to parse an integer value to the provided enum.
+        /// </summary>
+        /// <typeparam name="TEnum">The type of enum which the string should be parsed into.</typeparam>
+        /// <param name="value">The integer value to parse.</param>
         /// <param name="defaultValue">The default enum value to return if the value can't be parsed.</param>
         /// <returns>If parsing succeeds, then the parsed enum value will be returned. Otherwise the defaultvalue will be returned.</returns>
-        public static TEnum TryParseEnum<TEnum>(this int value, TEnum defaultValue) where TEnum : struct
+        public static TEnum? TryParseEnum<TEnum>(this int value, TEnum defaultValue) where TEnum : struct
         {
-            return EnumHelper.TryParseEnum(value, defaultValue);
+            return EnumHelper.TryParseEnum<TEnum>(value, defaultValue);
         }
     }
 }
--- CodeCube.Core/Helpers/EnumHelper.cs
+++ CodeCube.Core/Helpers/EnumHelper.cs
@@ -5,9 +5,9 @@ namespace CodeCube.Core.Helpers
     /// <summary>
     /// Helper class with enum methods.
     /// </summary>
-    public static class EnumHelper
+    internal static class EnumHelper
     {
-        public static TEnum TryParseEnum<TEnum>(int value, TEnum defaultValue) where TEnum : struct
+        internal static TEnum? TryParseEnum<TEnum>(int value, TEnum? defaultValue) where TEnum : struct
         {
             return TryParseEnum(value.ToString(), defaultValue);
         }
@@ -15,15 +15,17 @@ namespace CodeCube.Core.Helpers
         /// <summary>
         /// Try to parse a string value to the provided enum.
         /// </summary>
+        /// <remarks>Enum is always parsed case-insensitive!</remarks>
         /// <typeparam name="TEnum">The type of enum which the string should be parsed into.</typeparam>
         /// <param name="value">The string value to parse.</param>
         /// <param name="defaultValue">The default enum value to return if the value can't be parsed. Can be NULL</param>
         /// <returns>If parsing succeeds, then the parsed enum value will be returned. Otherwise the defaultvalue will be returned.</returns>
-        public static TEnum TryParseEnum<TEnum>(string value, TEnum defaultValue) where TEnum : struct
+        internal static TEnum? TryParseEnum<TEnum>(string value, TEnum? defaultValue) where TEnum : struct
         {
-            if (string.IsNullOrWhiteSpace(value)) return defaultValue;
+            if (string.IsNullOrWhiteSpace(value) && !defaultValue.HasValue) return default(TEnum);
+            if (string.IsNullOrWhiteSpace(value)) return defaultValue.Value;
 
-            if (Enum.TryParse(value, out TEnum result))
+            if (Enum.TryParse(value, true, out TEnum result))
             {
                 return result;
             }
